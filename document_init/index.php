afdf41f5cc876f793d0e1eaa6eb827d235d7ee53
Josue <115383359+Huayapa@users.noreply.github.com>
2023-10-10 10:43:06 -0500

    Add files via upload

---
+++ document_init/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es-pe">
+<head>
+    <!-- Cojunto de caracter universal -->
+    <meta charset="UTF-8">
+    <!-- Para que la pagina se visible -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Titulo para la pestaña del navegador -->
+    <title>Domentacion personalizado</title>
+    <!-- Vinculacion con CSS y fontello-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <!-- Utilizado para la declaracion del asunto en el sitio web -->
+    <meta name="subject" content="Insertar asunto"> <!--Añadir-->
+    <!-- Palabras claves para indicar la tematica de la web  -->
+    <meta name="keywords" content="Asunto"> <!--Añadir-->
+    <!-- Es la URL que considera reprensentativa en los navegadores -->
+    <link rel="canonical" href="https://www.midominio.com"> <!--Añadir-->
+    <!-- Es para que el navegador comienze a descargar este archivo primero -->
+    <link rel="preload" href="css/style.css" as="style">
+    <link rel="preload" href="css/style.css" as="style">
+    <!-- Icono para la pestaña -->
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon"> <!--Anadir-->
+</head>
+<body>
+</body>
+</html>
